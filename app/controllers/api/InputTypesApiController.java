9c5425bdc330c9b2bb5a542f7a02bc0a4da66a98
Dennis Oelkers <dennis@torch.sh>
2014-08-15 15:58:26 +0200

    Changing returned structure to be consistent with server api.

--- app/controllers/api/InputTypesApiController.java
+++ app/controllers/api/InputTypesApiController.java
@@ -4,10 +4,13 @@ import controllers.AuthenticatedController;
 import org.graylog2.restclient.lib.APIException;
 import org.graylog2.restclient.models.InputService;
 import org.graylog2.restclient.models.NodeService;
+import org.graylog2.restclient.models.api.responses.system.InputTypeSummaryResponse;
 import play.mvc.Result;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Dennis Oelkers <dennis@torch.sh>
@@ -23,7 +26,12 @@ public class InputTypesApiController extends AuthenticatedController {
     }
 
     public Result list() throws IOException, APIException {
-        return ok(json(inputService.getAllInputTypeInformation())).as("application/json");
+        Map<String, String> result = new HashMap<>();
+
+        for (Map.Entry<String, InputTypeSummaryResponse> entry : inputService.getAllInputTypeInformation().entrySet()) {
+            result.put(entry.getKey(), entry.getValue().name);
+        }
+        return ok(json(result)).as("application/json");
     }
 
     public Result get(String node_id, String type) throws IOException, APIException, NodeService.NodeNotFoundException {
